914362b8a5d5ecb2bb75d5e5dbf1b6017b86f999
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-01-19 12:28:46 +0530

    Add: Calculate Daily Employee Wage -Assume Wage per Hour is 20 -Assume Full Day Hour is 8

--- empWageComputationProblem.sh
+++ empWageComputationProblem.sh
@@ -1,10 +1,15 @@
-echo "Welcome to Check Employee is Present or Absent"
+echo "Welcome to Calcutale Daily Employee Wage"
 
 isPresent=1;
 randomCheck=$((RANDOM%2));
 if [ $isPresent -eq $randomCheck ]
 then
 echo "Employee is present"
+empRatePerHr=20
+empHrs=8;
+salary=$(($empHrs*$empRatePerHr))
+echo "salary="$salary
 else
 echo "Employee is absent"
+echo "salary="0
 fi
